54c911c1d428ecf3a5669b62b4980b0e07605667
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-11-07 16:59:10 +0600

    added adaptive to Footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -6,15 +6,15 @@ const Footer = () => {
     <div className={styles.container}>
       <div className={styles.row}>
         <div className={styles.column}>
-          {links.map((arr) => (
-            <div>
-              {arr.map((item) => (
-                <>
+          {links.map((arr, index) => (
+            <div key={index}>
+              {arr.map((item, index) => (
+                <div key={index}>
                   <h5>{item.title}</h5>
                   <a href={item.href}>
                     <p>{item.label}</p>
                   </a>
-                </>
+                </div>
               ))}
             </div>
           ))}
