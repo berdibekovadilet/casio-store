bf19c13cb4c3e5f4d512c794fd24ea0d88d456cc
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-11-07 16:18:58 +0600

    added mobile menu

--- src/layout/navbar/Navbar.tsx
+++ src/layout/navbar/Navbar.tsx
@@ -1,11 +1,18 @@
 import BottomNavbar from "./bottomNavbar/BottomNavbar";
 import TopNavbar from "./topNavbar/TopNavbar";
+import MobileMenu from "./mobileMenu/MobileMenu";
 
+import styles from "./Navbar.module.scss";
 const Navbar = () => {
   return (
-    <>
-      <TopNavbar /> <BottomNavbar />
-    </>
+    <div className={styles.wrapper}>
+      <div className={styles.desktop_nav}>
+        <TopNavbar /> <BottomNavbar />
+      </div>
+      <div className={styles.mobile_nav}>
+        <MobileMenu />
+      </div>
+    </div>
   );
 };
 
--- src/layout/navbar/mobileMenu/MobileMenu.tsx
+++ src/layout/navbar/mobileMenu/MobileMenu.tsx
@@ -1,9 +1,26 @@
-import styles from './MobileMenu.module.scss'
+import styles from "./MobileMenu.module.scss";
+import ShoppingCart from "assets/icons/ShoppingCart.svg";
+import BurgerMenu from "assets/icons/BurgerMenu.svg";
 
 const MobileMenu = () => {
   return (
-    <div>MobileMenu</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.nav_left}>
+        <div className={styles.menu_icon}>
+          <img src={BurgerMenu} className={styles.icon} alt="burgerMenuIcon" />
+        </div>
+        <h3 className={styles.logo}>Casio store</h3>
+      </div>
+      <div className={styles.nav_right}>
+        <a href="#/" className={styles.cart_link}>
+          <span>0 $</span>
+          <div className={styles.separator}></div>
+          <img src={ShoppingCart} className={styles.icon} alt="shopCartIcon" />
+          <span>0</span>
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default MobileMenu
+export default MobileMenu;
