d89202a385e637eb0c58deda575953b48f230e0c
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-11-16 18:50:29 +0600

    refactored Popular and Trends comps

--- src/components/popular/Popular.tsx
+++ src/components/popular/Popular.tsx
@@ -1,15 +1,38 @@
+import ProductService from "API/ProductService";
 import { Card } from "components/card/Card";
+import { CardSkeleton } from "components/card/cardSkeleton/CardSkeleton";
 import { SectionTitle } from "components/sectionTitle/SectionTitle";
+import { useEffect, useState } from "react";
 import styles from "./Popular.module.scss";
 
 export const Popular = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  async function fetchProducts() {
+    const products = await ProductService.getAll();
+    setItems(products);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const products = items
+    .slice(0, 8)
+    .map((obj: any) => <Card key={obj.id} {...obj} />);
+  const skeletons = [...new Array(8)].map((_, index) => (
+    <CardSkeleton key={index} />
+  ));
+
   return (
     <div className={styles.container}>
       <div className={styles.titleWrapper}>
         <SectionTitle title="Popular" />
       </div>
       <div className={styles.cardWrapper}>
-        {/* <Card /> */}
+        {isLoading ? skeletons : products}
       </div>
     </div>
   );
