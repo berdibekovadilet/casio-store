89a8d46abf1a4c53fb5b56d4727c55b8afac2735
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-11-23 13:41:09 +0600

    made layout Login page

--- src/components/sectionTitle/SectionTitle.tsx
+++ src/components/sectionTitle/SectionTitle.tsx
@@ -1,4 +1,5 @@
 import { Button } from "components/common/button/Button";
+import { Link } from "react-router-dom";
 import styles from "./SectionTitle.module.scss";
 
 type Props = {
@@ -9,9 +10,11 @@ export const SectionTitle: React.FC<Props> = ({ title }) => {
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>{title}</h2>
-      <Button appearance="primary" icon="right">
-        See All
-      </Button>
+      <Link to="shop">
+        <Button appearance="primary" icon="right">
+          See All
+        </Button>
+      </Link>
     </div>
   );
 };
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,21 @@
-import React from "react";
+import { Button, Input } from "components";
+import styles from "styles/pages/Login.module.scss";
 
 export const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <h2>Sign in to your profile</h2>
+      <form className={styles.container}>
+        <div className={styles.row}>
+          <p>Email</p>
+          <input type="text" />
+        </div>
+        <div className={styles.row}>
+          <p>Password</p>
+          <input type="password" />
+        </div>
+        <Button appearance="primary">Login</Button>
+      </form>
+    </div>
+  );
 };
